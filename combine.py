2c853386345945b3925c9aac584a41e4e1b91841
preich <preich@192.168.1.101>
2019-04-02 00:32:01 +0100

    merged with votes on mv1-3, iv1-2

--- combine.py
+++ combine.py
@@ -1,5 +1,27 @@
 import json
 import pandas as pd
+from fuzzywuzzy import fuzz
+import urllib.request
+import os.path
+
+def read_votes(key,id):
+    fname=str(id)+ '.json'
+    url='http://lda.data.parliament.uk/commonsdivisions/id/'+ fname
+    if not os.path.isfile(fname):
+        urllib.request.urlretrieve(url, fname)
+    body=open(fname).read()
+    js=json.loads(body)
+    results=[{'mp':x['memberPrinted']['_value'], key:x['type'].split('#')[1]} for x in js['result']['primaryTopic']['vote']]
+    return results
+
+def get_mp_match(mp,mps):
+    results=[]
+    for x in mps:
+        if str(x).rstrip().lstrip() != 'nan':
+            ratio=fuzz.ratio(mp, x)
+            results.append((x,ratio))
+    return sorted(results,key=lambda x: x[1],reverse=True)[0]
+
 def load_signatures(fname,column):
   js=json.loads(open(fname).read())
   pbc=js['data']['attributes']['signatures_by_constituency']
@@ -18,3 +40,52 @@ signatures_df=pro_eu_signatures_df.merge(nodeal_signatures_df, left_on=['mp','co
 combined=euref_df.merge(signatures_df, left_on='ons_code',right_on='ons_code')
 combined=combined.merge(electorate_df, left_on='constituency',right_on='constituency')
 combined.to_csv('combined.csv')
+
+divisions=dict()
+# confirmatory people's vote
+divisions['iv2_e']=1108906
+# customs union
+divisions['iv2_c']=1108905
+# commons market 2.0
+divisions['iv2_d']=1108904
+# parliamentary supremacy
+divisions['iv2_g']=1108907
+# no deal
+divisions['iv1_b']=1105521
+# common market 2.0
+divisions['iv1_d']=1105524
+# efta and eea
+divisions['iv1_h']=1105526
+# customs union
+divisions['iv1_j']=1105527
+# labour plan
+divisions['iv1_k']=1105529
+# revocation to avoid no deal
+divisions['iv1_l']=1105530
+# people's vote
+divisions['iv1_l']=1105532
+# preferential arrangements
+divisions['iv1_o']=1105533
+divisions['mv3']=1107737
+divisions['mv2']=1086876
+divisions['mv1']=1041567
+
+
+
+votes=pd.DataFrame()
+for key in divisions:
+    ret=pd.DataFrame(data=read_votes(key,divisions[key]))
+    if len(votes) == 0:
+        votes=ret
+    else:
+        votes=votes.merge(ret, left_on='mp', right_on='mp',how='outer')
+
+# in Hansard they use different naming convention for MPs
+mps_hansard=votes['mp'].tolist()
+mps_petitions=combined['mp'].tolist()
+mps_map=[(x, get_mp_match(x, mps_petitions)) for x in mps_hansard]
+
+vcombined=combined.merge(pd.DataFrame(data=[{'mp_name':x[0], 'mp':x[1][0], 'mp_name_match':x[1][1]} for x in mps_map]), left_on='mp',right_on='mp',how='left')
+vcombined=vcombined.merge(votes,left_on='mp_name',right_on='mp',how='left')
+vcombined.to_csv('vcombined.csv')
+
